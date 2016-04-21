c09b06c36f24b526c11c5c4b3d1a803e28f8fe15
Juliano Alves <juliano.caicara@gmail.com>
2016-04-21 15:11:14 -0300

    Commit 21/04/2016

---
+++ src/main/java/br/com/alura/loja/ClientTest.java
@@ -0,0 +1,51 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Projeto;
+
+public class ClientTest {
+
+	private HttpServer server;
+	
+	@Before
+	public void startaServidor(){
+		server = Servidor.inicializaServidor();
+	}
+	
+	@After
+	
+	public void mataServidor(){
+		server.stop();		
+	}
+	
+	
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado(){
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+	
+	@Test
+	public void testaQueBuscarUmProjetoTrazOProjetoEsperado(){
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/projetos").request().get(String.class);
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Minha Loja", projeto.getNome());
+	}
+}
